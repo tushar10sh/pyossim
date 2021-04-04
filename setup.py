f4b21151fb6b83e34f9f1cfd83bcf3045618dd27
Tushar Shukla <tushar.10sh@gmail.com>
2021-04-04 10:25:19 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setupsetup(
+    name=’mypythonlib’,
+    packages=find_packages(),
+    version=’0.1.0',
+    description=’My first Python library’,
+    author=’Me’,
+    license=’MIT’,
+)
